39916bba5bee118e685bcc2e004089e9c7cc441d
TeknikalDomain <jackwingeier@gmail.com>
2017-05-21 20:21:24 -0400

    Added Sysgen functionality.

--- Sysgen.cs
+++ Sysgen.cs
@@ -10,8 +10,30 @@ namespace untitledApplication
 {
     class Program
     {
+        static string filename;
         static void Main(string[] args)
         {
+            if (args.Length == 0)
+            {
+                Console.WriteLine("Error: No filename specified.");
+                return;
+            }
+            filename = args[0];
+            XmlDocument doc = new XmlDocument();
+            XmlDeclaration xmlDeclaration = doc.CreateXmlDeclaration("1.0", "UTF-8", null);
+            XmlElement root = doc.DocumentElement;
+            doc.InsertBefore(xmlDeclaration, root);
+            XmlElement XEXMFS = doc.CreateElement("XMFS");
+            doc.AppendChild(XEXMFS);
+            XmlElement XEConfig = doc.CreateElement("configuration");
+            XEXMFS.AppendChild(XEConfig);
+            XmlElement XEConfigHome = doc.CreateElement("home");
+            XEConfig.AppendChild(XEConfigHome);
+            XmlText XTConfigHomeCDATA = doc.CreateTextNode("/home");
+            XEConfigHome.AppendChild(XTConfigHomeCDATA);
+            XmlElement XEFilesystem = doc.CreateElement("filesystem");
+            XEXMFS.AppendChild(XEFilesystem);
+            doc.Save(filename);
         }
     }
 }
