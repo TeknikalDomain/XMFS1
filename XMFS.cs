e91ffe9fd19a2ffc4fc0d57b34f05c004c1bd473
TeknikalDomain <jackwingeier@gmail.com>
2017-05-21 23:09:10 -0400

    Finished basic navigation commands + added test filesystem.

--- XMFS.cs
+++ XMFS.cs
@@ -14,7 +14,7 @@ namespace untitledApplication
         static XmlElement workingElement;
         static string workingString = "/";
         static XmlDocument mainDoc;
-        static const string prompt = " $ ";
+        const string PROMPT = " $ ";
         static void Main(string[] args)
         {
             if (args.Length == 0)
@@ -28,8 +28,90 @@ namespace untitledApplication
                 Console.WriteLine("Error: Unable to access file {0}.", filename);
                 return;
             }
+            mainDoc = new XmlDocument();
             mainDoc.Load(filename);
             workingElement = (XmlElement)mainDoc.DocumentElement.ChildNodes.Item(1);
+            while (true)
+            {
+                Console.Write(workingString+PROMPT);
+                string input = Console.ReadLine();
+                if (input.Split(' ').First().ToUpper() == "QUIT" || input.Split(' ').First().ToUpper() == "LOGOUT" || input.Split(' ').First().ToUpper() == "EXIT")
+                {
+                    mainDoc.Save(filename);
+                    return;
+                }
+                switch (input.Split(' ').First().ToUpper())
+                {
+                    case "LS":
+                        for (int e = 0; e < workingElement.ChildNodes.Count; e++)
+                        {
+                            XmlElement cElm = (XmlElement)workingElement.ChildNodes.Item(e);
+                            Console.Write("{0}:{1}\t", e, cElm.GetAttribute("name"));
+                        }
+                        Console.Write("\n");
+                        break;
+
+                    case "MKDIR":
+                        string dirName = input.Split(' ')[1];
+                        bool isIntName = true;
+                        try
+                        {
+                            Int32.Parse(dirName);
+                        }
+                        catch (System.Exception)
+                        {
+                            isIntName = false;
+                        }
+                        if (isIntName)
+                        {
+                            Console.WriteLine("Error: names cannot be integers.");
+                            break;
+                        }
+                        XmlElement newFolder = mainDoc.CreateElement("folder");
+                        newFolder.SetAttribute("name", dirName);
+                        workingElement.AppendChild(newFolder);
+                        break;
+
+                    case "RMDIR":
+                        if (input.Split(' ')[1] == "-f") 
+                        {
+                            workingElement.RemoveChild(searchByID(input.Split(' ')[2]));
+                            break;
+                        }
+                        XmlElement folder = searchByID(input.Split(' ')[1]);
+                        if(folder == null)
+                        {
+                            Console.WriteLine("Error: folder does not exist.");
+                            break;
+                        }
+                        if(!folder.IsEmpty)
+                        {
+                            Console.WriteLine("Error: folder is not empty.");
+                            break;
+                        }
+                        workingElement.RemoveChild(folder);
+                        break;
+
+                    case "CD":
+                        if (input.Split(' ')[1] == "..")
+                        {
+                            DCU();
+                            break;
+                        }
+                        XmlElement nWDF = searchByID(input.Split(' ')[1]);
+                        if (nWDF == null)
+                        {
+                            Console.WriteLine("Error: folder does not exist.");
+                            break;
+                        }
+                        DCD(nWDF);
+                        break; 
+
+                    default:
+                        Console.WriteLine("Error: Unknown command: \"{0}\"", input);
+                        break;
+                }
+            }
         }
 
         #region cd functions
@@ -47,19 +129,20 @@ namespace untitledApplication
             return true;
         }
 
-        static bool DSD(XmlNode newElement)
+        static bool DCD(XmlNode newElement)
         {
             if (changeElement(newElement))
             {
-                workingString = workingString + newElement.LocalName;
+                XmlElement cElm = (XmlElement)newElement;
+                workingString = workingString + cElm.GetAttribute("name") + "/";
                 return true;
             }
             return false;
         }
 
-        static bool DSU()
+        static bool DCU()
         {
-            if (workingString = "/")
+            if (workingString == "/")
             {
                 return false;
             }
@@ -74,5 +157,31 @@ namespace untitledApplication
             return false;
         }
         #endregion
+
+        static XmlElement searchByName(string elementName)
+        {
+            for (int e = 0; e < workingElement.ChildNodes.Count; e++)
+            {
+                XmlElement cElm = (XmlElement)workingElement.ChildNodes.Item(e);
+                if (cElm.GetAttribute("name") == elementName)
+                {
+                    return cElm;
+                }
+            }
+            return null;
+        }
+
+        static XmlElement searchByID(string elementID)
+        {
+            try
+            {
+                XmlElement cElm = (XmlElement)workingElement.ChildNodes.Item(Int32.Parse(elementID));
+                elementID = cElm.GetAttribute("name");
+            }
+            catch (System.Exception)
+            {
+            }
+            return searchByName(elementID);
+        }
     }
 }
